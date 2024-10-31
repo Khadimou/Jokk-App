32a54f4d867c4b7b0b78a5d49413d1d48c199e89
Khadimou <57754498+Khadimou@users.noreply.github.com>
2024-10-31 21:56:06 +0100

    Update wsgi.py

--- SocialNetwork/wsgi.py
+++ SocialNetwork/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SocialNetwork.settings")
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'SocialNetwork.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
