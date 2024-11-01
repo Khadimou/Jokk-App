30297e684799d800667251ce01cc5fef926ca87f
Khadimou <57754498+Khadimou@users.noreply.github.com>
2024-11-01 07:45:35 +0100

    Update settings.py

--- SocialNetwork/settings.py
+++ SocialNetwork/settings.py
@@ -27,7 +27,7 @@ USE_S3 = False
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET", "default-secret-key")
+SECRET_KEY = os.environ.get("SECRET", "QILcNAXJyN")
  #"django-insecure-sx*x4m+=8_5dlui2=1l47+a+9t!lr2tp9$7(#k@rnteg8t&o4j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
